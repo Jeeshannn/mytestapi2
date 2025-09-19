a28fe5c4f0e30c16c45bab37c1c48ba95d85a012
Jeeshann <jeeshan.inamdaar@qtechnologiescorporation.com>
2025-09-19 17:19:23 +0530

    Update API endpoints / bug fixes

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI, UploadFile, File, Form, HTTPException
 from pydantic import BaseModel
 from typing import List, Optional
 
-app = FastAPI(title="User Identity API - Mocked ")
+app = FastAPI(title="User Identity API - Mocked New ")
 
 # -------------------------
 # Mock Database
